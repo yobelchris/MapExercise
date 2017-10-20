1dce3e3990db5acd85299303b1e40d4f68221449
Yobel Galih <yobelchris@gmail.com>
2017-10-20 23:04:03 +0700

    using street view

--- app/src/main/java/com/yobel/mapexercise/MainActivity.java
+++ app/src/main/java/com/yobel/mapexercise/MainActivity.java
@@ -1,46 +1,35 @@
 package com.yobel.mapexercise;
 
-import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapFragment;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.model.CameraPosition;
-import com.google.android.gms.maps.model.CircleOptions;
+import com.google.android.gms.maps.OnStreetViewPanoramaReadyCallback;
+import com.google.android.gms.maps.StreetViewPanorama;
+import com.google.android.gms.maps.StreetViewPanoramaFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.StreetViewPanoramaCamera;
 
-public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+public class MainActivity extends AppCompatActivity implements OnStreetViewPanoramaReadyCallback {
 
-    GoogleMap mMap;
-    boolean mapReady = false;
+    StreetViewPanorama panorama;
     LatLng jombang = new LatLng(-7.5622506, 112.2337198);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+        StreetViewPanoramaFragment streetViewPanoramaFragment = (StreetViewPanoramaFragment) getFragmentManager().findFragmentById(R.id.streetView);
+        streetViewPanoramaFragment.getStreetViewPanoramaAsync(this);
     }
 
     @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mapReady = true;
-        mMap = googleMap;
-        mMap.addCircle(new CircleOptions().center(new LatLng(-7.557808, 112.238784)).radius(30).strokeColor(Color.GREEN).fillColor(Color.argb(64, 0, 255, 0)));
-        changePosition(jombang);
-    }
-
-    void changePosition(LatLng target) {
-        CameraPosition cameraPosition = CameraPosition.builder()
-                .target(target)
-                .zoom(15)
+    public void onStreetViewPanoramaReady(StreetViewPanorama streetViewPanorama) {
+        panorama = streetViewPanorama;
+        panorama.setPosition(jombang);
+        StreetViewPanoramaCamera camera = StreetViewPanoramaCamera.builder()
                 .bearing(90)
+                .tilt(35)
                 .build();
-
-        mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition), 3000, null);
+        panorama.animateTo(camera, 3000);
     }
 }
