892a77bffb4b000ee4ff07382cb872bf13a346d3
Yobel Galih <yobelchris@gmail.com>
2017-10-20 22:40:04 +0700

    add circle

--- app/src/main/java/com/yobel/mapexercise/MainActivity.java
+++ app/src/main/java/com/yobel/mapexercise/MainActivity.java
@@ -8,8 +8,8 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.PolylineOptions;
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
@@ -29,17 +29,14 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     public void onMapReady(GoogleMap googleMap) {
         mapReady = true;
         mMap = googleMap;
-        mMap.addPolyline(new PolylineOptions().geodesic(true).add(new LatLng(-7.551187, 112.224978))
-                .add(new LatLng(-7.556107, 112.232671))
-                .add(new LatLng(-7.575390, 112.236555))
-                .add(new LatLng(-7.551187, 112.224978)));
+        mMap.addCircle(new CircleOptions().center(new LatLng(-7.557808, 112.238784)).radius(30));
         changePosition(jombang);
     }
 
     void changePosition(LatLng target) {
         CameraPosition cameraPosition = CameraPosition.builder()
                 .target(target)
-                .zoom(12)
+                .zoom(15)
                 .bearing(90)
                 .build();
 
